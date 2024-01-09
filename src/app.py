916b3b09f5bc217fa1df8cbf07ce899d631e2ec4
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-01-09 18:51:23 +0000

    falta los endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character, Favorites
+from ast import excepthandler
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +45,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    
+    #characters = Character.query.all()
+    #character_list = [{"id": character.id, "name": character.name, "eye_color": character.eye_color, "height": character.height} for character in characters] 
+    
+    #return jsonify(character_list), 200
+
+    #characters = Character.query.all()
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+    characters_list = [{"id": character.id, "name": character.name} for character in characters]
+    #return jsonify({'Characters': characters_list})
+    return jsonify(response_body), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,9 +4,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), nullable=False)
+    favorites = db.relationship("Favorites", back_populates="user")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +14,38 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username":self.username
             # do not serialize the password, its a security breach
         }
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dimension = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.String(100), nullable=False)
+    favorites = db.relationship("Favorites", back_populates="planet")
+
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    eye_color = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    favorites = db.relationship("Favorites", back_populates="character")
+
+
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User", back_populates="favorites")
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    planet = db.relationship("Planet", back_populates="favorites")
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    character = db.relationship("Character", back_populates="favorites")
+
+
+
+
